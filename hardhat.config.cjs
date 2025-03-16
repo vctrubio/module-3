5572507b86e389eb05ab61af6936e07215ba7d51
vctrubio <vctrubio@gmail.com>
2025-03-16 14:19:35 +0100

    hardhat config init

---
+++ hardhat.config.cjs
@@ -0,0 +1,11 @@
+require("@nomiclabs/hardhat-ethers");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+  },
+};
