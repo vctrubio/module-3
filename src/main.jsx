31215d2cd32d9fe9bad4f351a85f3f4d27cc6d8d
vctrubio <vctrubio@gmail.com>
2025-03-19 13:42:39 +0100

    vite installation

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <>
+      <h1>Hello, Vite + React!</h1>
+    </>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<App />);
