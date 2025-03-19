6d7ab960a50c81e71b0d2b239eba21f972bc8e43
vctrubio <vctrubio@gmail.com>
2025-03-19 11:24:24 +0100

    hardhat commonjs problems

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,8 +1,11 @@
 import fs from "fs";
 import { hardhat } from "../../config.js";
+import pkg from "hardhat";
+const { ethers } = pkg;
 
 async function main() {
   console.log(`🚀 Deploying to network: ${hardhat.network.name}`);
+  const contract = await ethers.getContractFactory("HouseUrban");
 }
 
 // Execute deployment
